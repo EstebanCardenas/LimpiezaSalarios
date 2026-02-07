67f6e6c11549265a85619aa5f61aaf5528b6d0e9
Nicolás Cárdenas <ne.cardenas@uniandes.edu.co>
2026-02-06 22:05:16 -0500

    Update clean df analysis

--- clean_df.py
+++ clean_df.py
@@ -5,4 +5,4 @@ df = pd.read_csv("data/clean_data.csv")
 pd.set_option('display.max_rows', None)    # None means "unlimited"
 pd.set_option('display.max_columns', None)
 
-print(df[(df['Country'] == 'Canada') & (df['Total Compensation COP'] > 1000000000)])
+print(df['Education Level'].isna().sum())
